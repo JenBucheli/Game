a5063cd8f82e5fc417212b61f914893ccec9df43
Jen <jenifer.bucheli@gmail.com>
2021-02-05 14:58:19 +0100

    javascript  update

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-const options=document.querySelectorAll("option");
+const options=document.querySelectorAll(".option");
 const score= document.getElementById("score");
 const result=document.getElementById( "result");
 const restart=document.getElementById("restart");
@@ -7,16 +7,60 @@ const scoreboard={
     computer:0
 }
 
+//play game
+function play(event){
+    //.target.id to select elements
+    let playerChoice =event.target.id;
+    let computer=computerChoice();
 
-//Find winner
+}
+//computer choice
 function computerChoice(){
-    const random=Math.random(p,c);
-    if (p===c){
+    let choice= Math.floor((Math.random()*options.length))
+    return computerChoice(choice)
+}
+//Find winner
+function findwinner(){
+
+    if (p === c) {
         return "draw";
-    } else if (p==="rock")
-        if(c==="paper"){
+
+    } else if (p === "rock") {
+        if (c === "paper"||"spock") {
             return "computer";
-        } else return "player";
+        } else {
+            return "player";
+        }
+
+    } else if (p === "paper") {
+        if (c === "scissors"||"lizard") {
+            return "computer";
+        } else {
+            return "player";
+        }
+
+    } else if (p === "scissors") {
+        if (c === "rock"||"spock") {
+            return "computer";
+        } else {
+            return "player";
+        }
+
+    } else if(p==="lizard"){
+        if(c==="scissors"||"rock"){
+            return "computer";
+        }else{
+            return "player";
+        }
+
+    } else if(p==="spock"){
+        if(c==="lizard"||"paper"){
+            return "computer";
+        }else {
+            return "player";
+        }
     }
 }
 
+//Event Listeners
+options.forEach(option =>option.addEventListener("click",play));
